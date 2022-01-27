4b7e195f9500aa3719c20c4cce6c3fd0b01ed2ff
Arthur Silva <arthur1silva1r@gmail.com>
2022-01-27 16:08:07 -0300

    funcionar o git pull

--- src/App.js
+++ src/App.js
@@ -7,21 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <ContextProvider>
-      <BrowserRouter>
-        <Route path="/" component={ Login } />
-      </BrowserRouter>
-    </ContextProvider>
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glas
-      </object>
-    </div>
+    <>
+      <ContextProvider>
+        <BrowserRouter>
+          <Route path="/" component={Login} />
+        </BrowserRouter>
+      </ContextProvider>
+    </>
   );
 }
 
