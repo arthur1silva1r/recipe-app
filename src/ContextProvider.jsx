8398c88c2f4d3bc26ea3f192a8a484772e37a53f
Gabriel Oliveira <gabrielfrox@gmail.com>
2022-01-27 16:21:25 -0300

    Setup para o Header

--- src/ContextProvider.jsx
+++ src/ContextProvider.jsx
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import MyContext from './MyContext';
 
+const USER_INITIAL_STATE = {
+  email: 'teste@gmail.com',
+  senha: '',
+};
 function ContextProvider({ children }) {
   const [foodsState, setFoodState] = useState({});
   const [drinkState, setDrinkState] = useState({});
-  const [userState, setUserState] = useState({});
+  const [userState, setUserState] = useState(USER_INITIAL_STATE);
 
   const providerObj = {
     foodsState,
