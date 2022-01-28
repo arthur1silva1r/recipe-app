fcf263d699ca61b1a49ada8b79e2c6f53177ac5f
alxedv <alexmaia.etel@gmail.com>
2022-01-27 21:04:03 -0300

    Adiciona as paginas /drinks, /explore/drinks e /explore/drinks/ingredients e por fim corrige erros referentes a importacoes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Explore from './pages/Explore';
 import Foods from './pages/Foods';
 import ExploreFoods from './pages/ExploreFoods';
-import ExploreIngredients from './pages/ExploreIngredients';
+import ExploreFoodIngredients from './pages/ExploreFoodIngredients';
 import ExploreNationalities from './pages/ExploreNationalities';
 import Details from './pages/Detalis';
 import ProcessRecipe from './pages/ProcessRecipe';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Profile from './pages/Profile';
+import Drinks from './pages/Drinks';
+import ExploreDrinks from './pages/ExploreDrinks';
+import ExploreDrinkIngredients from './pages/ExploreDrinkIngredients';
 
 function App() {
   return (
@@ -24,11 +27,22 @@ function App() {
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/explore" component={ Explore } />
           <Route exact path="/foods" component={ Foods } />
-          <Route exact path="/explore-foods" component={ ExploreFoods } />
-          <Route exact path="/explore-ingredients" component={ ExploreIngredients } />
+          <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/explore/foods" component={ ExploreFoods } />
+          <Route exact path="/explore/drinks" component={ ExploreDrinks } />
           <Route
             exact
-            path="/explore-nationalities"
+            path="/explore/foods/ingredients"
+            component={ ExploreFoodIngredients }
+          />
+          <Route
+            exact
+            path="/explore/drinks/ingredients"
+            component={ ExploreDrinkIngredients }
+          />
+          <Route
+            exact
+            path="/explore/foods/nationalities"
             component={ ExploreNationalities }
           />
           <Route exact path="/details" component={ Details } />
--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -2,8 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import MyContext from '../MyContext';
 
-export default function Explore() {
-  const name = 'Explore';
+export default function Drinks() {
+  const name = 'Drinks';
 
   const { titleHandler } = useContext(MyContext);
   useEffect(() => {
