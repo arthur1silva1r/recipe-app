6a9df4dd0a139af8dbf76f8999c75179eb4ca582
igor <igor.rafael.briano@gmail.com>
2022-01-31 21:09:35 -0300

    refatorado componente Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,75 +4,42 @@ import MyContext from '../MyContext';
 
 export default function Cards() {
   const { listOfResults, componentTitle } = useContext(MyContext);
-  let [id,
-    // thumb, str
-  ] = '';
+  let [id, thumb, str] = '';
   const MAX_ITEMS = 12;
   const url = componentTitle.toLowerCase();
 
   if (url === 'foods') {
     id = 'idMeal';
-    // thumb = 'strMealThumb';
-    // str = 'strMeal';
+    thumb = 'strMealThumb';
+    str = 'strMeal';
   } else if (url === 'drinks') {
     id = 'idDrink';
-    // thumb = 'strDrinkThumb';
-    // str = 'strDrink';
+    thumb = 'strDrinkThumb';
+    str = 'strDrink';
   }
 
-  const mealContent = () => (
-    <div>
-      {
-        listOfResults.map((element, index) => (
-          <Link
-            key={ element.strMeal }
-            to={ `${url}/${element[id]}` }
-          >
-            <div
-              data-testid={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ element.strMealThumb }
-                alt="recipe-thumbnail"
-                width="300px"
-              />
-              <h1 data-testid={ `${index}-card-name` }>{element.strMeal}</h1>
-            </div>
-          </Link>
-        ))
-      }
-    </div>
-  );
-  const drinkContent = () => (
-    <div>
-      {
-        listOfResults.slice(0, MAX_ITEMS).map((element, index) => (
-          <Link
-            key={ element.strDrink }
-            to={ `${url}/${element[id]}` }
-          >
-            <div
-              key={ element.strDrink }
-              data-testid={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ element.strDrinkThumb }
-                alt="recipe-thumbnail"
-                width="300px"
-              />
-              <h1 data-testid={ `${index}-card-name` }>{element.strDrink}</h1>
-            </div>
-          </Link>
-        ))
-      }
-    </div>
-  );
   return (
     <div className="cards">
       {
-        componentTitle === 'Foods' ? mealContent() : drinkContent()
+        listOfResults.slice(0, MAX_ITEMS).map((element, index) => (
+          <Link
+            key={ element[str] }
+            to={ `${url}/${element[id]}` }
+          >
+            <div
+              key={ element[str] }
+              data-testid={ `${index}-recipe-card` }
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ element[thumb] }
+                alt="recipe-thumbnail"
+                width="300px"
+              />
+              <h1 data-testid={ `${index}-card-name` }>{element[str]}</h1>
+            </div>
+          </Link>
+        ))
       }
     </div>
   );
