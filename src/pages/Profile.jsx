92cc09aae7fdcdabb92c6828ed6909da2c8426fd
alxedv <alexmaia.etel@gmail.com>
2022-02-03 20:22:30 -0300

    Corrige requisitos 10 e 11

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,6 +15,7 @@ export default function Profile() {
   }, [titleHandler]);
 
   const info = JSON.parse(localStorage.getItem('user'));
+  console.log(info);
 
   function clearStorage() {
     localStorage.clear();
@@ -24,32 +25,36 @@ export default function Profile() {
   return (
     <div>
       <Header />
-      <div className="user-info">
-        <h3 data-testid="profile-email">{info.email}</h3>
-        <div className="profile-buttons">
-          <button
-            type="button"
-            data-testid="profile-done-btn"
-            onClick={ () => history.push('/done-recipes') }
-          >
-            Done Recipes
-          </button>
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-            onClick={ () => history.push('/favorite-recipes') }
-          >
-            Favorite Recipes
-          </button>
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-            onClick={ clearStorage }
-          >
-            Logout
-          </button>
-        </div>
-      </div>
+      {
+        !info ? null : (
+          <div className="user-info">
+            <h3 data-testid="profile-email">{info.email}</h3>
+            <div className="profile-buttons">
+              <button
+                type="button"
+                data-testid="profile-done-btn"
+                onClick={ () => history.push('/done-recipes') }
+              >
+                Done Recipes
+              </button>
+              <button
+                type="button"
+                data-testid="profile-favorite-btn"
+                onClick={ () => history.push('/favorite-recipes') }
+              >
+                Favorite Recipes
+              </button>
+              <button
+                type="button"
+                data-testid="profile-logout-btn"
+                onClick={ clearStorage }
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        )
+      }
       <Footer />
     </div>
   );
